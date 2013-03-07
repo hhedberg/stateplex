22334b9b25b585870cc963f29010f70f582bdd42
Juha Hirvasniemi <hirvasniemi@suomi24.fi>
2013-03-07 12:52:53 +0200

    modified comments in iosource.h and spinlock.h

--- stateplex/core/iosource.h
+++ stateplex/core/iosource.h
@@ -25,6 +25,12 @@
 
 namespace Stateplex {
 
+/** 
+ * @brief Class IoSource.
+ *
+ * Class IoSource inherited from class Source. Used for...
+ */
+ 
 class IoSource : public Source {
 	Method mHandler;
 
@@ -60,50 +66,115 @@ public:
 /*** Inline implementations ***/
 
 namespace Stateplex {
+	
+/** 
+ * Constructor for IoSource class.
+ *
+ * @param *actor			desc
+ * @param fd				desc
+ * @param *handlerObject	desc
+ * @param *handlerFunction 	desc
+ */
 
 template<typename T>
 IoSource::IoSource(Actor *actor, int fd, T *handlerObject, void (T::*handlerFunction)(Source *source))
 	: Source(actor, fd, handlerObject, handlerFunction), mReadable(0), mWritable(0), mReadyToRead(0), mReadyToWrite(1), mReachedEof(0)
 { }
 
+/** 
+ * Destructor for IoSource class.
+ *
+ */
+ 
 inline IoSource::~IoSource()
 { }
 
+/** 
+ * Invoke
+ *
+ */
+ 
 inline void IoSource::invokeHandler()
 {
 	mHandler.invoke(this);
 }
 
+/** 
+ * Sets mode based on parameter values.
+ *
+ * @param readable	desc
+ * @param writable	desc
+ */
+ 
 inline void IoSource::setMode(bool readable, bool writable)
 {
 	mReadable = readable ? 1 : 0;
 	mWritable = writable ? 1 : 0;
 }
 
+/** 
+ * Checks if readable.
+ *
+ * @return	true if readable, otherwise false.
+ */
+ 
 inline bool IoSource::isReadable() const
 {
 	return !!mReadable;
 }
+
+/** 
+ * Checks if writable.
+ *
+ * @return	true if writable, otherwise false.
+ */
+ 
 inline bool IoSource::isWritable() const
 {
 	return !!mWritable;
 }
 
+/** 
+ * Checks if ready for reading.
+ *
+ * @return	true if ready, otherwise false.
+ */
+ 
 inline bool IoSource::isReadyToRead() const
 {
 	return !!mReadable && !!mReadyToRead;
 }
+
+/** 
+ * Checks if ready for writing.
+ *
+ * @return	true if ready, otherwise false.
+ */
 
 inline bool IoSource::isReadyToWrite() const
 {
 	return !!mWritable && !!mReadyToWrite;
 }
 
+/** 
+ * Checks if end-of-file has been reached.
+ *
+ * @return	true if eof has been reached, otherwise false.
+ */
+
 inline bool IoSource::hasReachedEof() const
 {
 	return !!mReachedEof;
 }
 
+/** 
+ * Sets handler.
+ * 
+ * @param *handlerObject	desc
+ * @param *handlerFunction	function that takes a pointer to IoSource type.
+ * @return	true if ready, otherwise false.
+ */
+ 
 template<typename T>
 void IoSource::setHandler(T *handlerObject, void (T::*handlerFunction)(IoSource *source))
 {
