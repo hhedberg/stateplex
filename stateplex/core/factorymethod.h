1b3d45e5ca55f3e605f7676025bbc8f64e63ee61
Henok shikur <henok.tenkir@gmail.com>
2013-03-13 10:02:58 +0200

    updated factorymethod.h

--- stateplex/core/factorymethod.h
+++ stateplex/core/factorymethod.h
@@ -47,15 +47,30 @@ public:
 
 namespace Stateplex {
 
+/** 
+ * A template function for constructor FactoryMethod class and handles conversation between unrelated types
+ * 
+ */
+
 template<typename Return>
 FactoryMethod<Return>::FactoryMethod()
 	: mObject(reinterpret_cast<Unknown *>(this)), mFunction(reinterpret_cast<Return *(Unknown::*)(Unknown *)>(&Method::warnUninitialisedMethod))
 { }
 
+/** 
+ * A template function for constructor FactoryMethod class.
+ */
+
 template<typename Return> template<typename Type, typename Argument>
 FactoryMethod<Return>::FactoryMethod(Type *object, Return *(Type::*function)(Argument *argument))
 	: mObject(reinterpret_cast<Unknown *>(object)), mFunction(reinterpret_cast<Return *(Unknown::*)(Unknown *)>(function))
 { }
+
+/**
+ * A template fucntion that sets any pointer to be converted to any other type pointer.
+ *
+ * @param mFunction	      is a member function.
+ */
 
 template<typename Return> template<typename Type, typename Argument>
 inline void FactoryMethod<Return>::set(Type *object, Return *(Type::*function)(Argument *argument))
@@ -63,6 +78,13 @@ inline void FactoryMethod<Return>::set(Type *object, Return *(Type::*function)(A
 	mObject = reinterpret_cast<Unknown *>(object);
 	mFunction = reinterpret_cast<Return *(Unknown::*)(Unknown *)>(function);
 }
+
+/**
+ * A template function that takes object and function and stores.
+ *
+ * @param mFunction	      is a member function.
+ * 
+ */
 
 template<typename Return> template<typename Argument>
 inline Return *FactoryMethod<Return>::invoke(Argument *argument) const
