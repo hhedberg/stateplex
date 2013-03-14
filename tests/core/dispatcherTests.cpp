b34535a9ef1124d92ae0bfc4ccd929a118b0c79f
Tapio Korvala <korvatap@gmail.com>
2013-03-14 13:44:22 +0200

    Made some templates for unit testing

---
+++ tests/core/dispatcherTests.cpp
@@ -0,0 +1,18 @@
+#include "gtest/gtest.h"
+
+class DispatcherTest : public testing::Test {
+protected:
+	
+	virtual void SetUp() 
+	{
+		
+	}
+
+	virtual void TearDown() { }
+
+};
+
+TEST_F(DispatcherTest, ActivateActorTest)
+{
+
+}
