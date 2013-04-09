c78b0e2364b5ed871d509f5e0cbea5b28c179bb2
Henrik Hedberg <hhedberg@innologies.fi>
2013-04-09 23:45:13 +0300

    EchoHbdpServer example: adding simple HBDP server example

---
+++ examples/echohbdpserver/echohbdpserver.h
@@ -0,0 +1,44 @@
+/*
+ * Stateplex - A server-side actor model library.
+ *
+ * examples/echohbdpserver/echohbdpserver.h
+ *
+ * (c) 2013 Henrik Hedberg <henrik.hedberg@innologies.fi>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1 as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Authors: Henrik Hedberg
+ */
+
+#ifndef INCLUDED_ECHO_HBDP_SERVER_H
+#define INCLUDED_ECHO_HBDP_SERVER_H
+
+#include <stateplex/core/actor.h>
+#include <stateplex/net/hbdpconnection.h>
+
+class EchoActor : public Stateplex::Actor {
+	Stateplex::HbdpServer *mHbdpServer;
+
+public:
+	EchoActor(Stateplex::Dispatcher *dispatcher);
+
+	Stateplex::HbdpConnection *instantiateHbdpConnection(const Stateplex::HbdpConnection::Embryo *embryo);
+};
+
+class EchoHbdpConnection : public Stateplex::HbdpConnection {
+
+protected:
+	virtual void receive();
+
+public:
+	EchoHbdpConnection(const Stateplex::HbdpConnection::Embryo *embryo);
+};
+
+#endif
