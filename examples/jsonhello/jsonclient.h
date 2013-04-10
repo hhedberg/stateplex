becff5d082790b6c81e3ee61b26ecc62d0fcad7a
Tapio Korvala <korvatap@gmail.com>
2013-04-10 14:03:23 +0300

    Added example for JSON & modified existing JSON implementation

---
+++ examples/jsonhello/jsonclient.h
@@ -0,0 +1,30 @@
+#ifndef INCLUDED_JSON_CLIENT_H
+#define INCLUDED_JSON_CLIENT_H
+
+#include <stateplex/core/actor.h>
+#include <stateplex/core/callbackmessage.h>
+
+#include <stateplex/json/jsondbactor.h>
+
+class JsonClient : public Stateplex::Actor {
+
+public:
+	JsonClient(Stateplex::Dispatcher *dispatcher);
+
+	void showResult(JsonDbActor::JsonMessage *message);
+};
+
+/*** Inline implementations ***/
+
+#include <iostream>
+
+JsonClient::JsonClient(Stateplex::Dispatcher *dispatcher)
+	: Actor(dispatcher)
+{ }
+
+void JsonClient::showResult(JsonDbActor::JsonMessage *message)
+{
+	message->result();
+}
+
+#endif
--- stateplex/json/jsondbactor.h
+++ stateplex/json/jsondbactor.h
@@ -6,35 +6,67 @@
 #include "../core/string.h"
 #include "jsonitem.h"
 
+enum JsonMessageType {
+	JSON_MESSAGE_TYPE_GET,
+	JSON_MESSAGE_TYPE_SET
+};
+
 class JsonDbActor : public Stateplex::Actor
 {
-
-
-
 public:
 
-	JsonDbActor();
-	~JsonDbActor();
+	JsonDbActor(Stateplex::Dispatcher*);
 
-	class JsonMessage: public Stateplex::CallbackMessage<JsonDbActor, JsonMessage> {
+	class JsonMessage : public Stateplex::CallbackMessage<JsonDbActor, JsonMessage> {
 		JsonMessageType mType;
 		//String *mParameter;
 		JsonItem *mResult;
 
 	public:
-		enum JsonMessageType {
-			JSON_MESSAGE_TYPE_GET,
-			JSON_MESSAGE_TYPE_SET
-		};
 
-		JsonMessage();
-		~JsonMessage();
+
+		template<typename Sender> JsonMessage(Sender *sender, JsonDbActor *receiver, JsonItem *item, void (Sender::*callbackFunction)(JsonMessage *message));
+		template<typename Sender> JsonMessage(Sender *sender, JsonDbActor *receiver, void (Sender::*callbackFunction)(JsonMessage *message));
+		void result() const;
 
 	protected:
-		void handle(Actor *sender, Actor *receiver);
+		void handle(Actor *sender, JsonDbActor *receiver);
+		
 	};
 
-
+	template<typename Sender> void getRootObject(Sender *sender, JsonItem *item, void (Sender::*callbackFunction)(JsonMessage *message));
 };
+
+/*** Inline implementations ***/
+
+#include <iostream>
+
+template<typename Sender> JsonDbActor::JsonMessage::JsonMessage(Sender *sender, JsonDbActor *receiver, JsonItem *item, void (Sender::*callbackFunction)(JsonMessage *message))
+	: Stateplex::CallbackMessage<JsonDbActor, JsonMessage>(sender, receiver, callbackFunction), mResult(item)
+{ }
+
+inline void JsonDbActor::JsonMessage::handle(Actor *sender, JsonDbActor *receiver)
+{	
+	std::cout << "SIMO";
+	JsonString *seppo = reinterpret_cast<JsonString *> (mResult);
+	std::cout << "KALLE";
+	std::cout << "SEPPO TESTI: " << seppo->key();
+	invokeCallback();
+}
+
+inline void JsonDbActor::JsonMessage::result() const
+{
+	std::cout << "SEPPO TESTI:";
+}
+
+inline JsonDbActor::JsonDbActor(Stateplex::Dispatcher *dispatcher)
+	: Actor(dispatcher)
+{ }
+
+template<typename Sender> void JsonDbActor::getRootObject(Sender *sender, JsonItem *item, void (Sender::*callbackFunction)(JsonMessage *message))
+{
+	JsonMessage *rootObject = new JsonMessage(sender, this, item, callbackFunction);
+	queueMessage(rootObject);
+}
 
 #endif
