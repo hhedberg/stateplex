994976b48aea65b8de38e881d66b2108f989d72e
Tapio Korvala <korvatap@gmail.com>
2013-05-10 00:36:14 +0300

    Refactored jsondouble and added comments

--- stateplex/json/jsondouble.cpp
+++ stateplex/json/jsondouble.cpp
@@ -1,5 +1,13 @@
 #include "jsondouble.h"
-#include <iostream>
+
+/**
+ * Constructor that takes assigns the owner of the 
+ * jsonstring and assigns the key and value for it.
+ *
+ * @param *owner	pointer to the owner actor.
+ * @param *key		wanted value for the key.
+ * @param number	wanted double for the value.
+ */
 
 JsonDouble::JsonDouble(Stateplex::Actor *owner, const char *key, double number)
 	: JsonItem(owner)
@@ -9,36 +17,78 @@ JsonDouble::JsonDouble(Stateplex::Actor *owner, const char *key, double number)
 	mType = JSON_DOUBLE;
 }
 
+/**
+ * Prints out the value of the json double.
+ * 
+ * @see JsonObject::traverse()
+ */
+
 void JsonDouble::traverse()
 {
 	std::cout << "\t" << '"' << mKey << '"' << " : " << mValue << std::endl;
 }
+
+/**
+ * Returns the value of the json double.
+ *
+ * @return		value of json double.
+ */
 
 double JsonDouble::value()
 {
 	return mValue;
 }
 
+/**
+ * Returns the key of the json double.
+ *
+ * @return		key of json double.
+ */
+
 const char *JsonDouble::key()
 {
 	return const_cast<const char*>(mKey);
 }
+
+/**
+ * Returns the type of json double.
+ * can be used to compare that it is
+ * actually a json double.
+ */
 
 JsonType JsonDouble::type()
 {
 	return mType;
 }
 
+/**
+ * Takes given decimal value as parameter
+ * and assigns it's value to json double's value.
+ *
+ * @param value		decimal to set.
+ */
 
 void JsonDouble::setValue(double value)
 {
 	mValue = value;
 }
 
+/**
+ * Increases the reference count by one.
+ * reference count is used to monitor/handle the usage
+ * of the JsonDouble.
+ */
+
 void JsonDouble::ref()
 {
 	mRefcount++;
 }
+
+/**
+ * Decreases the reference count by one.
+ * reference count is used to monitor/handle the usage
+ * of the JsonDouble.
+ */
 
 void JsonDouble::unref()
 {
@@ -47,12 +97,21 @@ void JsonDouble::unref()
 	}
 }
 
+/**
+ * Returns the reference count
+ *
+ * @return	reference count as Size32
+ * @see Stateplex::Size32
+ */
+
 Stateplex::Size32 JsonDouble::refcount() 
 {
 	return mRefcount;
 }
 
+/**
+ * Default desktructor for JsonDouble.
+ */
+
 JsonDouble::~JsonDouble()
-{
-        //dtor
-}
+{ }
--- stateplex/json/jsondouble.h
+++ stateplex/json/jsondouble.h
@@ -3,22 +3,28 @@
 
 #include "jsonitem.h"
 
+/**
+ * @brief JsonDouble contains double data.
+ *
+ * Class JsonDouble is a sub class of JsonItem. JsonDouble
+ * is a json item that can contain a decimal value.
+ * JsonDouble content can be modified after initialization.
+ */
+
 class JsonDouble : public JsonItem
 {
 public:
 	JsonDouble(Stateplex::Actor *owner, const char *key, double number);
-        virtual ~JsonDouble();
-	void traverse();
+        virtual ~JsonDouble();	
 	const char *key();
-	JsonType type();
 	double value();
 	void setValue(double value);
 	void unref();
+	void traverse();
+	JsonType type();
 	Stateplex::Size32 refcount();
-	
 protected:
 	void ref();
-	
 private:
 	double mValue;
 };
