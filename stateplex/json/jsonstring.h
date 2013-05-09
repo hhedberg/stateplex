c79e883a2e182aeb15471b46d9e4426643352608
Tapio Korvala <korvatap@gmail.com>
2013-05-10 00:05:34 +0300

    Commented jsonstring.h and refactored

--- stateplex/json/jsonstring.h
+++ stateplex/json/jsonstring.h
@@ -4,22 +4,29 @@
 #include "jsonitem.h"
 #include <iostream>
 
+/**
+ * @brief JsonStrings contains string data
+ *
+ * Class JsonString is a sub class of JsonItem. JsonString
+ * is a json item that can contain different kind of characters.
+ * JsonString can be reused and reset after initialization.
+ */
+
 class JsonString : public JsonItem
 {
 public:
 	JsonString(Stateplex::Actor *owner, const char *key, const char *value);
         virtual ~JsonString();
-	void traverse();
 	const char *key();
 	const char *value();
 	void setValue(Stateplex::String *str);
-	JsonType type();
+	void traverse();
 	void unref();
+	JsonType type();
 	Stateplex::Size32 refcount();
 	
 protected:
 	void ref();
-	
 private:
 	const char *mValue;
 };
